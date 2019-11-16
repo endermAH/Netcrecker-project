09dab4e0ee82786a39efee84a5258360ae02846c
Drogaytsev <dmitrydrogaytsev@gmail.com>
2019-11-17 01:28:33 +0300

    fix "host"

--- terraform/droplet3.tf
+++ terraform/droplet3.tf
@@ -8,9 +8,16 @@ resource "digitalocean_droplet" "liferay" {
     "${var.ssh_fingerprint}"
   ]
   connection {
+      host = "${digitalocean_droplet.liferay.ipv4_address}"
       user = "root"
       type = "ssh"
       private_key = "${file(var.pvt_key)}"
       timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt-get install -y python-pip python-yaml python-jinja2 python-httplib2 python-paramiko python-pkg-resources git"
+    ]
   }
 }
