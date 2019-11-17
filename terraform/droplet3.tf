d07c0cd1e16680f07912d744039ee50746d2d1ce
Drogaytsev <dmitrydrogaytsev@gmail.com>
2019-11-17 04:07:46 +0300

    install dependences

--- terraform/droplet3.tf
+++ terraform/droplet3.tf
@@ -1,23 +1,23 @@
 resource "digitalocean_droplet" "liferay" {
   image = "ubuntu-18-04-x64"
   name = "liferay"
-  region = "fra1"
+  region = "ams3"
   size = "s-1vcpu-3gb"
   private_networking = true
   ssh_keys = [
     "${var.ssh_fingerprint}"
   ]
   connection {
-      host = "${digitalocean_droplet.liferay.ipv4_address}"
-      user = "root"
-      type = "ssh"
-      private_key = "${file(var.pvt_key)}"
-      timeout = "2m"
+    host = "${digitalocean_droplet.liferay.ipv4_address}"
+    user = "root"
+    type = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    timeout = "2m"
   }
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update -y",
-      "sudo apt-get install -y python-pip python-yaml python-jinja2 python-httplib2 python-paramiko python-pkg-resources git"
+      "apt update -y",
+      "apt-get install -y python python3 python-pip python-yaml python-jinja2 python-httplib2 python-paramiko python-pkg-resources git",
     ]
   }
 }
