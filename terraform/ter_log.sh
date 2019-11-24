fd17556a55dbaa30b35685ce28227b68a0033701
John <gerakl00@gmail.com>
2019-11-24 19:41:45 +0300

    ter_log

--- terraform/ter_log.sh
+++ terraform/ter_log.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export DO_TOKEN="98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6"
+
 terraform $1 \
   -var "do_token=${DO_TOKEN}" \
   -var "pub_key=$HOME/.ssh/id_rsa.pub" \
