c48c7e3cf7c27553506ecba03f7a6149b6d903a1
John <gerakl00@gmail.com>
2019-11-24 23:59:30 +0300

    parse

--- terraform/parse_ip.py
+++ terraform/parse_ip.py
@@ -7,6 +7,6 @@ f = open('terraform.tfstate')
 json_string = f.read()
 parsed_string = json.loads(json_string)
 
-for i in range(0,2):
+for i in range(0,3):
     if parsed_string['resources'][i]['name'] == sys.argv[1]:
         print(parsed_string['resources'][i]['instances'][0]['attributes']['ipv4_address'])
