264c084185cc44bb0320d53257c77f2b287ca1e4
John <gerakl00@gmail.com>
2019-11-24 22:22:41 +0300

    parcer v1

---
+++ terraform/parse_ip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import json
+
+f = open('terraform.tfstate')
+json_string = f.read()
+parsed_string = json.loads(json_string)
+print(parsed_string['resources']['instances'])
+
+print(parsed_string['resources'][0]['instances'][0]['attributes']['ipv4_address'])
--- terraform/ter_log.sh
+++ terraform/ter_log.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DO_FING=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
+DO_FING=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | cut -c 5-)
 DO_TOKEN="98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6"
+
+echo
 
 terraform $1 \
   -var "do_token=${DO_TOKEN}" \
