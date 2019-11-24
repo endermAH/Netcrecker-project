69e9b491b8ea0ede2bd5324de247a11e09959b59
John <gerakl00@gmail.com>
2019-11-24 19:59:30 +0300

    fl

--- terraform/droplet3.tf
+++ terraform/droplet3.tf
@@ -21,3 +21,8 @@ resource "digitalocean_droplet" "liferay" {
     ]
   }
 }
+
+resource "digitalocean_floating_ip_assignment" "foobar" {
+  ip_address = "178.128.142.117"
+  droplet_id = digitalocean_droplet.liferay.id
+}
