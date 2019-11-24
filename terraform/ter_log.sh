9b70b63c60013304ac16cb7e4b180f7c93e0c8ca
John <gerakl00@gmail.com>
2019-11-24 21:20:55 +0300

    люта

--- terraform/ter_log.sh
+++ terraform/ter_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export DO_FING="1e:5b:78:b9:30:cc:35:f8:b0:ba:0c:99:99:38:28:16"
-export DO_TOKEN="98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6"
+DO_FING=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
+DO_TOKEN="98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6"
 
 terraform $1 \
   -var "do_token=${DO_TOKEN}" \
