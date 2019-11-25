a5e42a3d0f301e2fa02bf7909757d41b70e9b912
Drogaytsev <dmitrydrogaytsev@gmail.com>
2019-11-26 00:58:25 +0300

    unzip

--- terraform/droplet2.tf
+++ terraform/droplet2.tf
@@ -17,7 +17,7 @@ resource "digitalocean_droplet" "jenkins" {
   provisioner "remote-exec" {
     inline = [
       "apt update -y",
-      "apt-get install -y ansible python python3 python-pip python-yaml python-jinja2 python-httplib2 git",
+      "apt-get install -y ansible python python3 python-pip python-yaml python-jinja2 python-httplib2 git unzip",
     ]
   }
 }
