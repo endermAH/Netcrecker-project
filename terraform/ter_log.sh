d3f9eea825657eba2f030a33b03704e0a7724f14
John <gerakl00@gmail.com>
2019-11-26 13:24:45 +0300

    ter_log correct

--- terraform/ter_log.sh
+++ terraform/ter_log.sh
@@ -3,8 +3,6 @@
 DO_FING=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | cut -c 5-)
 DO_TOKEN="98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6"
 
-echo
-
 terraform $1 -auto-approve \
   -var "do_token=${DO_TOKEN}" \
   -var "pub_key=$HOME/.ssh/id_rsa.pub" \
