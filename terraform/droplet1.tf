5d238900b4a8ed3a8d3c3ec67d2743e44530518f
John <gerakl00@gmail.com>
2019-11-28 11:30:07 +0300

    fixed inventory

--- terraform/droplet1.tf
+++ terraform/droplet1.tf
@@ -21,3 +21,8 @@ resource "digitalocean_droplet" "psql" {
     ]
   }
 }
+
+resource "digitalocean_floating_ip_assignment" "psql_fip" {
+  ip_address = "134.209.137.88"
+  droplet_id = digitalocean_droplet.jenkins.id
+}
