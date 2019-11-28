7b2f0986eb7b7ba2608b5bb0cd68a946fb557997
peoplearestrong <56271382+peoplearestrong@users.noreply.github.com>
2019-11-28 16:46:17 +0300

    test new java

--- terraform/ter_log.sh
+++ terraform/ter_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DO_FING=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | cut -c 5-)
-DO_TOKEN="98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6"
+DO_TOKEN="224507c855781e2bb5ad6f1eca0237b7f856a0b96d2a0797b0771c6a0136c04b"
 
 terraform $1 -auto-approve \
   -var "do_token=${DO_TOKEN}" \
