21ae7521c1429e08db3d4cd158016eb27ac191dc
peoplearestrong <56271382+peoplearestrong@users.noreply.github.com>
2019-12-04 14:04:28 +0300

    changed ip droplets

--- terraform/ter_log.sh
+++ terraform/ter_log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DO_FING=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | cut -c 5-)
-DO_TOKEN="98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6"
+DO_TOKEN="81de493f0c712c4eff53e9b6cda57e1ef3bbbc1ee04e6f855c8b31e4693e6b84"
 
 terraform $1 -auto-approve \
   -var "do_token=${DO_TOKEN}" \
