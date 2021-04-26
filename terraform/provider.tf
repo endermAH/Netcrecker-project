5131d34675c4a6935f2eb1b78700ef8546d45045
Evgeny Kuryatov <34351907+endermAH@users.noreply.github.com>
2021-04-26 03:44:52 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,5 +4,5 @@ variable "pvt_key" {}
 variable "ssh_fingerprint" {}
 
 provider "digitalocean" {
-  token = var.do_token
+  token = 98fcb6662e0ecdef1501f564390a5f42b9cbefe7689ca99a0d091b3b117c72c6
 }
